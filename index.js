f77bdf44a034efe3c9743363bc6c2fef6d59868e
aishachoudery <aichoudery31@gmail.com>
2018-08-08 18:24:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Write your code in this file!
 
-function scuberGreetingForFeet(ride) {
+var ride = 
+function scuberGreetingForFeet() {
   if ride <= 400
     console.log("This one is on me!")
 }
